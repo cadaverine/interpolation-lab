60acee47374f6cbc1fed0f516f3f20ee9aea5dbe
cadaverine <kennville@gmail.com>
2019-11-07 00:37:49 +0300

    feat(table.go): add 'getNeighbors' function

--- table.go
+++ table.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type Table struct {
 	Xs []float64
 	Ys []float64
@@ -22,4 +24,46 @@ func CreateConfig(xArr []float64, reducer func(float64) float64) *Config {
 	}
 
 	return (*Config)(CreateTable(xArr, yArr))
+}
+
+func getNeighbors(values []float64, currentValue float64, pointsNum int) ([]float64, error) {
+	valuesNum := len(values)
+
+	if pointsNum > valuesNum {
+		return nil, errors.New("length error")
+	}
+
+	if pointsNum == valuesNum {
+		clone := make([]float64, len(values))
+		copy(clone, values)
+
+		return clone, nil
+	}
+
+	rightOffset := pointsNum / 2
+	leftOffset := rightOffset + pointsNum%2
+
+	var from, to int
+
+	for i, value := range values {
+		if value >= currentValue {
+			if i < leftOffset {
+				from = 0
+				to = pointsNum - 1
+			} else if valuesNum-1-i < rightOffset {
+				to = valuesNum - 1
+				from = to - (pointsNum - 1)
+			} else {
+				from = i - leftOffset
+				to = i + rightOffset
+			}
+
+			break
+		}
+	}
+
+	result := make([]float64, pointsNum)
+	copy(result, values[from:to])
+
+	return result, nil
 }
