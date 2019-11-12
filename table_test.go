cfcd5538a43fab8852908a7fd9ee46a730f8aeac
cadaverine <kennville@gmail.com>
2019-11-12 20:49:33 +0300

    test(table_test.go): update 'Test_getNeighbors'

--- table_test.go
+++ table_test.go
@@ -1,27 +1,56 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
+type inputData struct {
+	values       []float64
+	currentValue float64
+	pointsNum    int
+}
+
+type outputData struct {
+	from int
+	to   int
+	err  bool
+}
+
 type TestCase struct {
-	values         []float64
-	currentValue   float64
-	pointsNum      int
-	expectedResult []float64
-	hasError       bool
+	input  *inputData
+	output *outputData
 }
 
 func generateTestCases() []*TestCase {
 	testCases := []*TestCase{
-		&TestCase{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 1.5, 5, []float64{1.0, 2.0, 3.0, 4.0, 5.0}, false},
-		&TestCase{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 6.9, 2, []float64{6.0, 7.0}, false},
-		&TestCase{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 4.1, 3, []float64{3.0, 4.0, 5.0}, false},
-		&TestCase{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 4.1, 4, []float64{3.0, 4.0, 5.0, 6.0}, false},
-		&TestCase{[]float64{1.0, 2.0, 3.0}, 0.0, 2, []float64{1.0, 2.0}, false},
-		&TestCase{[]float64{1.0, 2.0, 3.0}, 3.1, 1, nil, true},
-		&TestCase{[]float64{1.0, 2.0, 3.0}, 3.1, 4, nil, true},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 1.5, 5},
+			&outputData{0, 4, false},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 6.9, 2},
+			&outputData{5, 6, false},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 4.1, 3},
+			&outputData{3, 5, false},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0}, 4.1, 4},
+			&outputData{2, 5, false},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0}, 0.0, 2},
+			&outputData{0, 1, false},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0}, 3.1, 1},
+			&outputData{0, 0, true},
+		},
+		&TestCase{
+			&inputData{[]float64{1.0, 2.0, 3.0}, 3.1, 4},
+			&outputData{0, 0, true},
+		},
 	}
 
 	return testCases
@@ -31,15 +60,15 @@ func Test_getNeighbors(t *testing.T) {
 	testCases := generateTestCases()
 
 	for _, c := range testCases {
-		result, err := getNeighbors(c.values, c.currentValue, c.pointsNum)
+		from, to, err := getNeighbors(c.input.values, c.input.currentValue, c.input.pointsNum)
 
-		if !reflect.DeepEqual(result, c.expectedResult) {
-			t.Errorf("Error: results is not equal. Result: %v, Expected: %v", result, c.expectedResult)
+		if from != c.output.from || to != c.output.to {
+			t.Errorf("Error: results is not equal.\nResult from: %v, to: %v\nExpected from: %v, to: %v", from, to, c.output.from, c.output.to)
 		}
 
-		if err == nil && c.hasError {
+		if err == nil && c.output.err {
 			t.Errorf("Error: error is expected.")
-		} else if err != nil && !c.hasError {
+		} else if err != nil && !c.output.err {
 			t.Errorf("Error: error is not expected.")
 		}
 	}
